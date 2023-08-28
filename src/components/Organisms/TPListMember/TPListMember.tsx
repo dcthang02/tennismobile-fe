951840243e47df9e5f152a1a340fae0a1911a2a5
Thang <bokimtan123@gmail.com>
2023-08-28 13:40:12 +0700

    add member list filter

--- src/components/Organisms/TPListMember/TPListMember.tsx
+++ src/components/Organisms/TPListMember/TPListMember.tsx
@@ -1,8 +1,17 @@
+import TPRow from "@/components/Atom/TPRow";
 import TPWrapper from "@/components/Atom/TPWrapper";
+import TPButton from "@/components/Molecules/TPButton";
 import TPMemberItem from "@/components/Molecules/TPMemberItem";
+import TPSearchBar from "@/components/Molecules/TPSearchBar";
+import FilterIcon from "assets/icon/filter.svg";
 import useNavigation from "@/hooks/useNavigation";
-import React, { useCallback } from "react";
-import { FlatList } from "react-native";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { FlatList, View, StyleSheet, Pressable } from "react-native";
+import TPModal from "@/components/Molecules/TPModal";
+import { COLORS } from "@/constant/colors";
+import TPText from "@/components/Atom/TPText";
+import TPRadio from "@/components/Atom/TPRadio";
+import TPDivide from "@/components/Atom/TPDivide";
 
 const MEMBERS = [
   {
@@ -24,7 +33,7 @@ const MEMBERS = [
     name: "Mai Siêu Phong",
     avatar:
       "https://static.vecteezy.com/system/resources/thumbnails/001/503/756/small/boy-face-avatar-cartoon-free-vector.jpg",
-    rank: 5,
+    rank: 3,
   },
   {
     id: "003",
@@ -45,7 +54,7 @@ const MEMBERS = [
     name: "Chu Bá Thông",
     avatar:
       "https://static.vecteezy.com/system/resources/thumbnails/001/503/756/small/boy-face-avatar-cartoon-free-vector.jpg",
-    rank: 4,
+    rank: 5,
   },
 ];
 
@@ -53,7 +62,39 @@ type TPListMemberProps = {
   navigation: any;
 };
 
+type TypeFilter = {
+  name: "desc" | "asc";
+  rank: "desc" | "asc";
+};
+
+const getPlayerFirstName = (name: string) => {
+  return name.split(" ").slice(-1)[0];
+};
+
 export const TPListMember = ({ navigation }: TPListMemberProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const [filterModal, setFilterModal] = useState<TypeFilter>({
+    name: "asc",
+    rank: "asc",
+  });
+  const [filter, setFilter] = useState<TypeFilter>({
+    name: "asc",
+    rank: "asc",
+  });
+  useEffect(() => {
+    setFilterModal(filter);
+  }, [filter]);
+  const members = useMemo(() => {
+    return MEMBERS.sort(
+      (a, b) =>
+        (filter.rank === "asc" ? a.rank - b.rank : b.rank - a.rank) ||
+        (filter.name === "asc"
+          ? getPlayerFirstName(a.name).localeCompare(getPlayerFirstName(b.name))
+          : getPlayerFirstName(b.name).localeCompare(
+              getPlayerFirstName(a.name)
+            ))
+    );
+  }, [filter]);
   const { handleNavigate } = useNavigation(navigation);
   const handleNavigateMemberInfo = useCallback(
     (id: string, name: string) => {
@@ -61,10 +102,118 @@ export const TPListMember = ({ navigation }: TPListMemberProps) => {
     },
     [handleNavigate]
   );
+
+  const handleChangeFilter = useCallback(
+    (key: "rank" | "name", value: "asc" | "desc") => {
+      setFilterModal((prevFilter) => {
+        const filterObj = { ...prevFilter };
+        filterObj[key] = value;
+        return filterObj;
+      });
+    },
+    []
+  );
+
+  const handleApplyFilter = useCallback(() => {
+    setShowModal(false);
+    setFilter(filterModal);
+  }, [filterModal]);
+
+  const renderSelectGroup = useCallback(
+    (
+      title: string,
+      options: { text: string; value: "asc" | "desc" }[],
+      key: "rank" | "name"
+    ) => {
+      return (
+        <>
+          <TPWrapper gap={8}>
+            <TPText variant="heading6">{title}</TPText>
+            {options.map((item, i) => (
+              <Pressable
+                key={`option-filter-${item.text}-${i}`}
+                onPress={() => handleChangeFilter(key, item.value)}
+              >
+                <TPRow style={styles.filterRow}>
+                  <TPRadio active={item.value === filterModal[key]} />
+                  <TPText variant="body14">{item.text}</TPText>
+                </TPRow>
+              </Pressable>
+            ))}
+          </TPWrapper>
+          <TPDivide />
+        </>
+      );
+    },
+    [filterModal, handleChangeFilter]
+  );
+
+  const renderModal = useCallback(() => {
+    return (
+      <TPModal
+        isShow={showModal}
+        onCloseModal={() => {
+          setShowModal(false);
+          setFilterModal(filter);
+        }}
+        headerTitle="Lọc"
+        headerRight={
+          <TPButton
+            title="Xóa"
+            buttonType="text"
+            backgroundColor="transparent"
+            color={COLORS.green[600]}
+            onPress={() => {
+              setFilter({ name: "asc", rank: "asc" });
+              setShowModal(false);
+            }}
+          />
+        }
+      >
+        <TPWrapper gap={10}>
+          {renderSelectGroup(
+            "Hạng",
+            [
+              { text: "Từ cao đến thấp", value: "asc" },
+              { text: "Từ thấp đến cao", value: "desc" },
+            ],
+            "rank"
+          )}
+          {renderSelectGroup(
+            "Tên",
+            [
+              { text: "Từ A - Z", value: "asc" },
+              { text: "Từ Z - A", value: "desc" },
+            ],
+            "name"
+          )}
+          <TPButton title="Áp dụng" size="large" onPress={handleApplyFilter} />
+        </TPWrapper>
+      </TPModal>
+    );
+  }, [renderSelectGroup, showModal, handleApplyFilter]);
+
   return (
-    <TPWrapper>
+    <TPWrapper gap={15}>
+      {renderModal()}
+      <TPRow style={styles.searchBarRow}>
+        <View style={{ flex: 1 }}>
+          <TPSearchBar
+            placeholder="Tìm kiếm thành viên"
+            backgroundColor="transparent"
+          />
+        </View>
+        <TPButton
+          title="f"
+          // startIcon={<FilterIcon />}
+          size="tiny"
+          buttonType="text"
+          backgroundColor="transparent"
+          onPress={() => setShowModal(true)}
+        />
+      </TPRow>
       <FlatList
-        data={MEMBERS}
+        data={members}
         renderItem={({ item, index }) => (
           <TPMemberItem
             player={item}
@@ -80,3 +229,15 @@ export const TPListMember = ({ navigation }: TPListMemberProps) => {
     </TPWrapper>
   );
 };
+
+const styles = StyleSheet.create({
+  searchBarRow: {
+    gap: 15,
+    alignItems: "center",
+    paddingHorizontal: 16,
+  },
+  filterRow: {
+    gap: 8,
+    alignItems: "center",
+  },
+});
--- src/screens/members/MemberScreen.tsx
+++ src/screens/members/MemberScreen.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import FilterIcon from "assets/icon/filter.svg";
-
 import useNavigation from "../../hooks/useNavigation";
 import { MemberProps } from "@/utils/createProps";
 import TPBackground from "@/components/Atom/TPBackgroud";
 import TPWrapper from "@/components/Atom/TPWrapper";
 import TPTabHeader from "@/components/Molecules/TPTabHeader";
-import TPRow from "@/components/Atom/TPRow";
-import TPSearchBar from "@/components/Molecules/TPSearchBar";
-import TPButton from "@/components/Molecules/TPButton";
 import TPListMember from "@/components/Organisms/TPListMember";
 
 const MemberScreen = ({ navigation }: MemberProps) => {
@@ -18,32 +12,10 @@ const MemberScreen = ({ navigation }: MemberProps) => {
     <TPBackground>
       <TPWrapper paddingHorizontal={16} marginBottom={16}>
         <TPTabHeader title="Danh sách thành viên" />
-        <TPRow style={styles.searchBarRow}>
-          <View style={{ flex: 1 }}>
-            <TPSearchBar
-              placeholder="Tìm kiếm thành viên"
-              backgroundColor="transparent"
-            />
-          </View>
-          <TPButton
-            title="f"
-            // startIcon={<FilterIcon />}
-            size="tiny"
-            buttonType="text"
-            backgroundColor="transparent"
-          />
-        </TPRow>
       </TPWrapper>
       <TPListMember navigation={navigation} />
     </TPBackground>
   );
 };
-
-const styles = StyleSheet.create({
-  searchBarRow: {
-    gap: 15,
-    alignItems: "center",
-  },
-});
 
 export default MemberScreen;
