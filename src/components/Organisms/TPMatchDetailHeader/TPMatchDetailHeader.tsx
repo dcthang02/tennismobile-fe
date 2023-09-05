20e63e23aeec723b38e66fe8bd66775542374794
Thang <bokimtan123@gmail.com>
2023-09-05 15:46:07 +0700

    update image carousel

--- src/components/Organisms/TPMatchDetailHeader/TPMatchDetailHeader.tsx
+++ src/components/Organisms/TPMatchDetailHeader/TPMatchDetailHeader.tsx
@@ -1,6 +1,7 @@
 import TPIcon from "@/components/Atom/TPIcon";
 import TPText from "@/components/Atom/TPText";
 import TPButton from "@/components/Molecules/TPButton";
+import TPImageCarousel from "@/components/Molecules/TPImageCarousel";
 import { COLORS } from "@/constant/colors";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Image, StyleSheet, View, Dimensions, FlatList } from "react-native";
@@ -14,33 +15,10 @@ export const TPMatchDetailHeader = ({
   images,
   onPressButtonBack,
 }: TPMatchDetailHeaderProps) => {
-  const [imageIndex, setImageIndex] = useState(0);
-
-  const handleSwipeImage = useCallback((offset: number) => {
-    setImageIndex(Math.ceil(offset / Dimensions.get("window").width));
-  }, []);
-
   if (images.length === 0) return null;
   return (
     <View style={styles.container}>
-      <FlatList
-        data={images}
-        renderItem={({ item, index }) => (
-          <Image
-            source={{ uri: item }}
-            width={Dimensions.get("window").width}
-            height={250}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        decelerationRate={"fast"}
-        snapToAlignment="start"
-        snapToInterval={Dimensions.get("window").width}
-        onMomentumScrollEnd={(e) => {
-          handleSwipeImage(e.nativeEvent.contentOffset.x);
-        }}
-      />
+      <TPImageCarousel images={images} />
 
       <View style={styles.buttonView}>
         <TPButton
@@ -57,11 +35,6 @@ export const TPMatchDetailHeader = ({
           onPress={onPressButtonBack}
         />
       </View>
-      <View style={styles.indexView}>
-        <TPText variant="small-semibold" color={COLORS.charcoal.white}>
-          {imageIndex + 1}/{images.length}
-        </TPText>
-      </View>
     </View>
   );
 };
@@ -74,14 +47,5 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 50,
     left: 5,
-  },
-  indexView: {
-    position: "absolute",
-    bottom: 10,
-    right: 15,
-    backgroundColor: COLORS.black.fade,
-    paddingVertical: 4,
-    paddingHorizontal: 12,
-    borderRadius: 20,
   },
 });
