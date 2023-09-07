04be44695adfab9f808cff82cd59938042ac9d3b
Thang <bokimtan123@gmail.com>
2023-09-07 15:44:18 +0700

    add upload image hook

--- src/screens/account/AccountProfileEditScreen.tsx
+++ src/screens/account/AccountProfileEditScreen.tsx
@@ -1,7 +1,7 @@
 import TPBackground from "@/components/Atom/TPBackgroud";
 import TPHeader from "@/components/Molecules/TPHeader";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { AccountEditProfileProps } from "@/utils/createProps";
 import TPTextInput from "@/components/Molecules/TPTextInput";
 import TPWrapper from "@/components/Atom/TPWrapper";
@@ -12,21 +12,31 @@ import TPRow from "@/components/Atom/TPRow";
 
 import { AntDesign } from "@expo/vector-icons";
 import { COLORS } from "@/constant/colors";
+import useUploadImage from "@/hooks/useUploadImage";
 
 const avatar_uri =
   "https://static.wikia.nocookie.net/powerrangers/images/0/0f/COCX-41908.jpg/revision/latest?cb=20221127143637";
 
 const AccountProfileEditScreen = ({ navigation }: AccountEditProfileProps) => {
+  const { singleFile, selectFile } = useUploadImage();
+
+  console.log(singleFile);
+
   return (
     <TPBackground>
       <TPHeader headerTitle="Tài khoản" />
       <TPWrapper paddingHorizontal={16} gap={10}>
-        <TPRow style={styles.avatarBox}>
-          <TPAvatar uri={avatar_uri} size="large" />
-          <TPRow style={styles.iconCamera}>
-            <AntDesign name="camerao" size={16} color="black" />
+        <Pressable onPress={selectFile}>
+          <TPRow style={styles.avatarBox}>
+            <TPAvatar
+              uri={singleFile?.assets ? singleFile.assets?.[0].uri : avatar_uri}
+              size="large"
+            />
+            <TPRow style={styles.iconCamera}>
+              <AntDesign name="camerao" size={16} color="black" />
+            </TPRow>
           </TPRow>
-        </TPRow>
+        </Pressable>
         <TPTextInput label="Họ tên" inputType="text" />
         <TPDatePicker />
         <TPSelection
