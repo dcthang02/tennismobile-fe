3aa7d5080be678678f504ba50c2797674f77ee0f
Thang <bokimtan123@gmail.com>
2023-10-23 07:58:35 +0700

    integrate api my profile

--- src/components/Organisms/TPPlayerProfile/TPPlayerProfile.tsx
+++ src/components/Organisms/TPPlayerProfile/TPPlayerProfile.tsx
@@ -9,66 +9,64 @@ import { TPPlayerUtility } from "./TPPlayerUtility";
 import { TPPlayerFreeTime } from "./TPPlayerFreeTime";
 import TPChangePassword from "../TPChangePassword";
 
-const MEMBER = {
-  avatar:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1DeyZNqRdLF9WiyJOo7YQW5HxbSp3F6tNQQ&usqp=CAU",
-  rank: 10,
-  level: 3,
-  age: 25,
-  club: {
-    id: "123",
-    name: "Club Tech Fox",
-  },
-  statistic: {
-    matches: {
-      all: 125,
-      won: 50,
-      lost: 75,
-    },
-    leagues: {
-      matches: 45,
-      won: 15,
-      lost: 30,
-    },
-    score: {
-      sum: 375,
-      won: 250,
-      average: 180,
-    },
-  },
-  utility: {
-    "Hãng vợt": ["Wilson", "tennis-ball"],
-    "Thông số vợt": ["305G", "tennis-racket"],
-    "Cước sử dụng": ["Zone", "transform-origin"],
-    Backhand: ["Hai tay", "gloves"],
-    Forehand: ["Tay thuận", "dead-hand"],
-    "Tay thuận": ["Tay phải", "tennis"],
-    "Hãng bóng": ["Dunlop", "tennis-ball"],
-    "Quần áo": ["Hugo Boss", "tshirt"],
-    "Size quần áo": ["M", "resize-x"],
-    "Hãng giày": ["New Balance", "gym-shoes"],
-    "Size giày": ["44", "resize-y"],
-  },
-  freeTime: [
-    [true, false, false],
-    [false, true, true],
-    [false, false, true],
-    [true, false, false],
-    [false, true, false],
-    [false, true, true],
-    [false, true, true],
-  ],
-};
+// const member = {
+//   avatar:
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1DeyZNqRdLF9WiyJOo7YQW5HxbSp3F6tNQQ&usqp=CAU",
+//   rank: 10,
+//   level: 3,
+//   age: 25,
+//   club: {
+//     id: "123",
+//     name: "Club Tech Fox",
+//   },
+//   statistic: {
+//     matches: {
+//       all: 125,
+//       won: 50,
+//       lost: 75,
+//     },
+//     leagues: {
+//       matches: 45,
+//       won: 15,
+//       lost: 30,
+//     },
+//     score: {
+//       sum: 375,
+//       won: 250,
+//       average: 180,
+//     },
+//   },
+//   utility: {
+//     "Hãng vợt": ["Wilson", "tennis-ball"],
+//     "Thông số vợt": ["305G", "tennis-racket"],
+//     "Cước sử dụng": ["Zone", "transform-origin"],
+//     Backhand: ["Hai tay", "gloves"],
+//     Forehand: ["Tay thuận", "dead-hand"],
+//     "Tay thuận": ["Tay phải", "tennis"],
+//     "Hãng bóng": ["Dunlop", "tennis-ball"],
+//     "Quần áo": ["Hugo Boss", "tshirt"],
+//     "Size quần áo": ["M", "resize-x"],
+//     "Hãng giày": ["New Balance", "gym-shoes"],
+//     "Size giày": ["44", "resize-y"],
+//   },
+//   freeTime: [
+//     [true, false, false],
+//     [false, true, true],
+//     [false, false, true],
+//     [true, false, false],
+//     [false, true, false],
+//     [false, true, true],
+//     [false, true, true],
+//   ],
+// };
 
 type TPPlayerProfileProps = {
-  id: string;
-  name: string;
   editable?: boolean;
+  member: any;
 };
 
 export const TPPlayerProfile = ({
-  id,
-  name,
+  member,
   editable = false,
 }: TPPlayerProfileProps) => {
   return (
@@ -77,21 +75,21 @@ export const TPPlayerProfile = ({
       showsVerticalScrollIndicator={false}
     >
       <TPPlayer
-        name={name}
-        avatar={MEMBER.avatar}
-        rank={MEMBER.rank}
-        level={MEMBER.level}
-        age={MEMBER.age}
-        clubName={MEMBER.club.name}
+        name={member.name}
+        avatar={member.avatar}
+        rank={member.rank}
+        level={member.level}
+        age={member.age}
+        clubName={member.club.name}
         editable={editable}
       />
       <TPPlayerStatistic
-        matches={MEMBER.statistic.matches}
-        leagues={MEMBER.statistic.leagues}
-        score={MEMBER.statistic.score}
+        matches={member.statistic.matches}
+        leagues={member.statistic.leagues}
+        score={member.statistic.score}
       />
-      <TPPlayerUtility utility={MEMBER.utility} editable={editable} />
-      <TPPlayerFreeTime freeTime={MEMBER.freeTime} editable={editable} />
+      {/* <TPPlayerUtility utility={member.utility} editable={editable} /> */}
+      <TPPlayerFreeTime freeTime={member.freeTime} editable={editable} />
       {editable && <TPChangePassword />}
     </ScrollView>
   );
--- src/screens/account/AccountScreen.tsx
+++ src/screens/account/AccountScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "@/context/AuthContext";
 import TPBackground from "@/components/Atom/TPBackgroud";
@@ -7,16 +7,19 @@ import TPWrapper from "@/components/Atom/TPWrapper";
 import TPPlayerProfile from "@/components/Organisms/TPPlayerProfile";
 import { AccountProps } from "@/utils/createProps";
 import useNavigation from "@/hooks/useNavigation";
+import useMe from "@/hooks/useMe";
 
 const AccountScreen = ({ navigation }: AccountProps) => {
-  const { signout } = useContext(AuthContext);
+  const { myData } = useMe();
+
+  if (!myData) return null;
   return (
     <TPBackground>
       <TPWrapper paddingHorizontal={16} marginBottom={16}>
         <TPTabHeader title="Tài khoản" />
       </TPWrapper>
       <TPWrapper paddingHorizontal={16} marginBottom={75}>
-        <TPPlayerProfile id="123" name="Gia Cát Lượng" editable />
+        <TPPlayerProfile member={myData.me} editable />
       </TPWrapper>
     </TPBackground>
   );
